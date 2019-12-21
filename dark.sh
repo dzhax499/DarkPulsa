e7e80f0a323163d49955ab6aa7c39249334c8e98
dzhax499 <57004084+dzhax499@users.noreply.github.com>
2019-12-22 06:32:08 +0700

    Add files via upload

---
+++ dark.sh
@@ -0,0 +1,47 @@
+echo -e '\e[1;33m'"________                __   __________      __                  
+\______ \ _____ _______|  | _\______   \__ __|  |   ___________   
+ |    |  \\__  \\_  __ \  |/ /|     ___/  |  \  |  /  ___/\__  \  
+ |        \/ __ \|  | \/    < |    |   |  |  /  |__\___ \  / __ \_
+/_______  (____  /__|  |__|_ \|____|   |____/|____/____  >(____  /
+        \/     \/           \/                         \/      \/ "
+sleep 2
+echo "[i] Initializing.."
+sleep 3
+echo "[i] Connecting to driver.."
+sleep 2
+echo -e '\e[1;30m'"[o] Connected!"
+sleep 2
+echo -e '\e[1;33m'"[i] Connecting to Telephone.."
+sleep 2
+echo -e '\e[1;30m'"[o] Connected!"
+sleep 1
+echo "Checking.."
+sleep 5
+echo -e '\e[1;31m'"[!] Not subscribed YtDzakir.."
+sleep 2
+echo "Subscribing.."
+python yt.py
+sleep 9
+echo "Checking you subscribed.."
+sleep 5
+echo -e '\e[1;31m'"[!]Not SubscriBeD, subscribed again!"
+sleep 3
+echo "Subscribing again.."
+python yt.py
+sleep 9
+echo "checking.."
+sleep 9
+echo "Thanks!"
+clear
+echo -e '\e[1;33m'"________                __   __________      __
+\______ \ _____ _______|  | _\______   \__ __|  |   ___________                                                          |    |  \\__  \\_  __ \  |/ /|     ___/  |  \  |  /  ___/\_
+_  \
+ |        \/ __ \|  | \/    < |    |   |  |  /  |__\___ \  / __ \_
+/_______  (____  /__|  |__|_ \|____|   |____/|____/____  >(____   
+          \/     \/           \/                         \/      \/ "
+
+sleep 3
+read -p "No.Telp : " no;
+sleep 5
+echo "The NumBER YOUR CALLING is Fail "
+sleep 2
---
+++ yt.py
@@ -0,0 +1,3 @@
+import os
+os.system('xdg-open https://www.youtube.com/channel/UCTqd51rwFkarJF4jnyvOHtQ')
+
